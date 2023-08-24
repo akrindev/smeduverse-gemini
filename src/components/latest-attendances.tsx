01aa358d804997c9ae689ec882cc75df5636149e
akrindev <akrinmin@gmail.com>
2023-08-24 09:06:50 +0700

    fix: upgrade package

--- src/components/latest-attendances.tsx
+++ src/components/latest-attendances.tsx
@@ -1,20 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAttendance } from "@/store/attendance";
 import { Attendance } from "@/types/attendances";
-import { Block, List, ListItem } from "konsta/react";
+import { Block, List, ListItem, Toast } from "konsta/react";
 import { format } from "date-fns";
 
 export default function LatestAttendances() {
+  // state toast
+  const [toastLeftOpened, setToastLeftOpened] = useState(false);
   const [attendances, fetchAttendances] = useAttendance<
     [Attendance[], () => void]
   >((state) => [state.attendances, state.fetchAttendances]);
-
-  console.log("att", attendances);
 
   // fetch attendances using use effect
   useEffect(() => {
     fetchAttendances();
   }, [fetchAttendances]);
+
+  const onItemClicked = (id: number) => {
+    console.log("item clicked", id);
+    setToastLeftOpened(true);
+  };
 
   return (
     <>
@@ -25,6 +30,7 @@ export default function LatestAttendances() {
             <ListItem
               key={attendance.id}
               link
+              onClick={() => onItemClicked(attendance.id)}
               chevronMaterial={false}
               title={attendance.student.fullname}
               after={format(new Date(attendance.attendance_date), "HH:mm")}
@@ -33,6 +39,10 @@ export default function LatestAttendances() {
           ))}
         </List>
       </Block>
+
+      <Toast position='left' opened={toastLeftOpened}>
+        <div className='shrink'>item clicked</div>
+      </Toast>
     </>
   );
 }
