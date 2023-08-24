2a16a3af7506b8de372c8fffeec9337aa6165ae2
akrindev <akrinmin@gmail.com>
2023-08-24 09:10:48 +0700

    fix: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,18 @@
-import type { Config } from "tailwindcss";
+// import konstaConfig config
 const konstaConfig = require("konsta/config");
 
-const config: Config = {
+// wrap config with konstaConfig config
+module.exports = konstaConfig({
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
-};
-
-module.exports = konstaConfig(config);
+});
