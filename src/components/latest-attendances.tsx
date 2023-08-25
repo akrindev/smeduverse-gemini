239e78379fcaf45e0477ce6fd223ab08b70291dd
akrindev <akrinmin@gmail.com>
2023-08-25 09:37:45 +0700

    fix: deleting kehadiran

--- src/components/latest-attendances.tsx
+++ src/components/latest-attendances.tsx
@@ -1,24 +1,90 @@
 import { useEffect, useState } from "react";
 import { useAttendance } from "@/store/attendance";
-import { Attendance } from "@/types/attendances";
-import { Block, BlockTitle, List, ListItem, Toast } from "konsta/react";
+import {
+  Block,
+  BlockTitle,
+  Dialog,
+  DialogButton,
+  List,
+  ListItem,
+  Preloader,
+  Toast,
+} from "konsta/react";
 import { format } from "date-fns";
+
+import success from "../assets/success.json";
+import error from "../assets/error.json";
+import Lottie from "react-lottie";
 
 export default function LatestAttendances() {
   // state toast
-  const [toastLeftOpened, setToastLeftOpened] = useState(false);
-  const [attendances, fetchAttendances] = useAttendance<
-    [Attendance[], () => void]
-  >((state) => [state.attendances, state.fetchAttendances]);
+  const [deletedSuccessful, setDeletedSuccessful] = useState(false);
+  // dialog to confirm delete
+  const [dialogOpened, setDialogOpened] = useState(false);
+  // is loading
+  const [isLoading, setIsLoading] = useState(false);
+  // selected id
+  const [selectedId, setSelectedId] = useState<number | null>(null);
+
+  const [attendances, fetchAttendances, deleteAttendance] = useAttendance(
+    (state) => [
+      state.attendances,
+      state.fetchAttendances,
+      state.deleteAttendance,
+    ]
+  );
 
   // fetch attendances using use effect
   useEffect(() => {
     fetchAttendances();
   }, [fetchAttendances]);
 
+  // lottie default
+  // these options are lottie options
+  // it will be used to render lottie animation
+  const errorOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: error,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  const successOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: success,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   const onItemClicked = (id: number) => {
     console.log("item clicked", id);
-    setToastLeftOpened(true);
+    setDialogOpened(true);
+    // setToastLeftOpened(true);
+    setSelectedId(id);
+  };
+
+  // confirmed delte
+  const onConfirmedDelete = () => {
+    const successSound = new Audio("/sounds/successful.ogg");
+
+    setIsLoading(true);
+    deleteAttendance(selectedId!).finally(() => {
+      setDialogOpened(false);
+      // setToastLeftOpened(true);
+      setSelectedId(null);
+      setIsLoading(false);
+      setDeletedSuccessful(true);
+
+      // play sound
+      successSound.play();
+
+      setTimeout(() => {
+        setDeletedSuccessful(false);
+      }, 3000);
+    });
   };
 
   return (
@@ -46,9 +112,34 @@ export default function LatestAttendances() {
         )}
       </Block>
 
-      <Toast position='left' opened={toastLeftOpened}>
-        <div className='shrink'>item clicked</div>
-      </Toast>
+      {/* dialog to confirm delete */}
+      <Dialog
+        opened={dialogOpened}
+        onBackdropClick={() => setDialogOpened(false)}
+        title='Hapus kehadiran ini?'
+        content='Kehadiran akan dihapus'
+        buttons={[
+          <DialogButton key='cancel' onClick={() => setDialogOpened(false)}>
+            Batal
+          </DialogButton>,
+          <DialogButton
+            key='delete'
+            onClick={onConfirmedDelete}
+            disabled={isLoading}>
+            {isLoading ? <Preloader /> : "Hapus"}
+          </DialogButton>,
+        ]}
+      />
+
+      {/* dialog alert */}
+      <Dialog
+        opened={deletedSuccessful}
+        onBackdropClick={() => setDeletedSuccessful(false)}
+        title={
+          <Lottie options={successOptions} speed={1} height={120} width={120} />
+        }
+        content={"Kehadiran berhasil dihapus"}
+      />
     </>
   );
 }
--- src/store/attendance.ts
+++ src/store/attendance.ts
@@ -1,29 +1,37 @@
-// it is a store management file for attendance
 import { api } from "@/hooks/auth";
 import { AttendancesResponse } from "@/types/attendances";
+import { AxiosPromise, AxiosResponse } from "axios";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-// interface for attendance
 interface Attendance {
   attendances: Array<any>;
-  fetchAttendances: () => Promise<any>;
+  fetchAttendances: () => Promise<AxiosResponse<AttendancesResponse>>;
+  deleteAttendance: (id: number) => Promise<AxiosResponse<AttendancesResponse>>;
 }
 
-// create a store for attendance
 export const useAttendance = create<Attendance>((set, get) => ({
   attendances: [],
-  //   fetch attendance from api
   fetchAttendances: async () => {
     const res = await api
       .get<AttendancesResponse>("/api/attendance/apel/latest")
-      .then((res) => {
-        // console the response
-        // console.log(res.data);
-
+      .then((res: AxiosResponse<AttendancesResponse>) => {
         set({
           attendances: res.data.attendances.data,
         });
+        return res;
+      });
+
+    return res;
+  },
+  deleteAttendance: async (id: number) => {
+    const res = await api
+      .delete(`/api/attendance/apel/delete/${id}`)
+      .then((res: AxiosResponse<AttendancesResponse>) => {
+        // populate attendances
+        get().fetchAttendances();
+
+        return res;
       });
 
     return res;
