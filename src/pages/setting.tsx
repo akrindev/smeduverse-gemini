d0a1a3bc99c2072f97f140395a18cdab9f9a006b
akrindev <akrinmin@gmail.com>
2023-08-26 11:30:47 +0700

    fix: setting pages

--- src/pages/setting.tsx
+++ src/pages/setting.tsx
@@ -1,14 +1,48 @@
 import KonstaLayouts from "@/components/konsta-layouts";
 import WithNavbar from "@/components/with-navbar";
-import { Block, Navbar, Page } from "konsta/react";
+import { useAuth } from "@/hooks/auth";
+import {
+  Block,
+  BlockTitle,
+  List,
+  ListButton,
+  ListItem,
+  Navbar,
+  Page,
+} from "konsta/react";
 
 export default function Setting() {
+  const { user, logout } = useAuth({
+    middleware: "auth",
+    redirectIfAuthenticated: "/home",
+  });
+
+  console.log(user);
+
   return (
     <KonstaLayouts>
       <Page>
         <Navbar title='Setting' />
         <WithNavbar>
-          <Block strong>Setting Page</Block>
+          <BlockTitle>Profile</BlockTitle>
+          {user && (
+            <List strongIos outlineIos>
+              <ListItem
+                header='Nama Lengkap'
+                title={user.data.identity.fullname}
+              />
+              {user.data.teacher! ? (
+                <ListItem header='NIY' title={user.data.identity.niy} />
+              ) : (
+                <ListItem header='NIS' title={user.data.identity.nipd} />
+              )}
+            </List>
+          )}
+          <List inset strong>
+            <ListButton className='k-color-brand-red' onClick={logout}>
+              Keluar
+            </ListButton>
+          </List>
         </WithNavbar>
       </Page>
     </KonstaLayouts>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,17 @@ const konstaConfig = require("konsta/config");
 
 // wrap config with konstaConfig config
 module.exports = konstaConfig({
+  konsta: {
+    colors: {
+      primary: "#007aff",
+      "brand-primary": "#007aff",
+      "brand-red": "#ff3b30",
+      "brand-green": "#4cd964",
+      "brand-yellow": "#ffcc00",
+      "brand-purple": "#9c27b0",
+      "brand-blue": "#2196f3",
+    },
+  },
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
