c20da328f074842ce7ec052bdde4929fc92804a9
akrindev <akrinmin@gmail.com>
2023-09-16 08:46:33 +0700

    update font and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,9 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Roboto } from "next/font/google";
+import { Inter } from "next/font/google";
 
-export const roboto = Roboto({
+export const roboto = Inter({
   subsets: ["latin"],
-  weight: ["400", "500", "700"],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
