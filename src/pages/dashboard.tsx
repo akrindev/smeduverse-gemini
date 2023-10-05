8e63d3d496a2e6c5c92c6242d89d1f724ddd571b
akrindev <akrinmin@gmail.com>
2023-10-05 10:28:08 +0700

    Refactor dashboard page with improved UI/UX

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,85 +1,22 @@
 // Importing necessary libraries and components
 import KonstaLayouts from "@/components/konsta-layouts";
-import QRCode from "@/components/qrcode";
-import { Page, Block, Navbar, Dialog, BlockTitle } from "konsta/react";
-import { useEffect, useState } from "react";
-import { api } from "@/hooks/auth";
-import Lottie from "react-lottie";
-import { useAttendance } from "@/store/attendance";
-import LatestAttendances from "@/components/latest-attendances";
+import { Page, Navbar } from "konsta/react";
+import { useAuth } from "@/hooks/auth";
 import Head from "next/head";
 import WithNavbar from "@/components/with-navbar";
-import { successOptions, errorOptions } from "@/lib/lottie-options";
+import { useRouter } from "next/router";
 
 // Main function for the Dashboard page
 export default function DashboardPage() {
-  // State variables
-  const [tokenized, setTokenized] = useState(""); // For storing tokenized QR code
-  const [alertOpened, setAlertOpened] = useState(false); // For handling alert dialog
-  const [message, setMessage] = useState(""); // For storing messages
-  const [results, setResults] = useState([]); // For storing results
-  const [isError, setIsError] = useState(false); // For error handling
+  const { user } = useAuth({
+    middleware: "auth",
+    redirectIfAuthenticated: "/login",
+  });
 
-  // Function to fetch attendances
-  const fetchAttendances = useAttendance((state) => state.fetchAttendances);
+  const router = useRouter();
 
-  // Callback function for successful QR code scan
-  const qrCodeSuccessCallback = (qrCodeMessage: string) => {
-    // console.log("scanned", qrCodeMessage);
-    setIsError(false);
+  if (!user) return <>Loading . . .</>;
 
-    // Updating results
-    setResults((prev) => [...prev, qrCodeMessage]);
-
-    // Checking for duplicate scans
-    if (tokenized !== qrCodeMessage) {
-      setTokenized(qrCodeMessage);
-
-      // API call to store attendance
-      api
-        .post("/api/attendance/apel/store", { nis: qrCodeMessage })
-        .then((response) => {
-          setAlertOpened(true);
-          setMessage(response.data.message);
-
-          fetchAttendances();
-        })
-        .catch((error) => {
-          setAlertOpened(true);
-          setMessage(error.response.data.message);
-          setIsError(true);
-        });
-    }
-  };
-
-  // Effect to remove last scanned QR code after 3 seconds
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTokenized((prev) => "");
-    }, 3000);
-    return () => clearInterval(interval);
-  }, []);
-
-  // Effect to handle alert dialog and play sounds
-  useEffect(() => {
-    const successSound = new Audio("/sounds/successful.ogg");
-    const errorSound = new Audio("/sounds/failure.ogg");
-    if (alertOpened) {
-      setTimeout(() => {
-        setAlertOpened(false);
-      }, 3000);
-
-      // Playing sounds based on success or failure
-      if (isError) {
-        errorSound.play();
-      }
-      if (!isError) {
-        successSound.play();
-      }
-    }
-  }, [alertOpened, isError]);
-
-  // Rendering the Dashboard page
   return (
     <KonstaLayouts>
       <Head>
@@ -88,40 +25,34 @@ export default function DashboardPage() {
       <Page>
         <WithNavbar>
           <Navbar title='Dashboard' />
-          <BlockTitle>Scan Kehadiran</BlockTitle>
-          <Block>
-            <QRCode
-              fps={10}
-              qrbox={300}
-              aspectRatio={1.0}
-              disableFlip={false}
-              verbose={false}
-              qrCodeSuccessCallback={qrCodeSuccessCallback}
-            />
-          </Block>
-          <div className='mt-5'>
-            <LatestAttendances />
+          <div className='p-5 grid grid-cols-1 gap-3'>
+            <div
+              className='col-span-1 hover:scale-105 cursor-pointer duration-400'
+              onClick={() => router.push("/scan/apel")}>
+              <div className='bg-white rounded-lg shadow-lg p-6'>
+                <h2 className='text-lg font-medium text-gray-900'>
+                  Presensi Apel
+                </h2>
+                <p className='mt-2 text-sm text-gray-500'>
+                  Ketuk untuk mengelola catatan kehadiran apel siswa.
+                </p>
+              </div>
+            </div>
+            <div
+              className='col-span-1 hover:scale-105 cursor-pointer duration-400'
+              onClick={() => router.push("/scan/ekskul")}>
+              <div className='bg-white rounded-lg shadow-lg p-6'>
+                <h2 className='text-lg font-medium text-gray-900'>
+                  Presensi Ekskul
+                </h2>
+                <p className='mt-2 text-sm text-gray-500'>
+                  Ketuk untuk mengelola kehadiran kegiatan ekstrakurikuler.
+                </p>
+              </div>
+            </div>
           </div>
         </WithNavbar>
       </Page>
-
-      <Dialog
-        opened={alertOpened}
-        onBackdropClick={() => setAlertOpened(false)}
-        title={
-          isError ? (
-            <Lottie
-              options={errorOptions}
-              speed={0.5}
-              height={180}
-              width={180}
-            />
-          ) : (
-            <Lottie options={successOptions} height={180} width={180} />
-          )
-        }
-        content={message}
-      />
     </KonstaLayouts>
   );
 }
